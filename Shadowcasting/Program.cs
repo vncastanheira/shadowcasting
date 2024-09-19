25f367b8511a02b5426c321e844c7fa432f33838
Vinicius <castvn@proton.me>
2024-09-19 20:44:43 -0300

    Init

---
+++ Shadowcasting/Program.cs
@@ -0,0 +1,36 @@
+﻿string[,] map = new[,] {
+    { "#", "#", "#", "#", "#", "#" },
+    { "#", ".", ".", ".", ".", "#" },
+    { "#", ".", "#", ".", ".", "#" },
+    { "#", "#", "#", ".", ".", "#" },
+    { "#", ".", ".", ".", ".", "#" },
+    { "#", ".", "#", "#", ".", "#" },
+    { "#", "@", ".", ".", ".", "#" },
+    { "#", ".", ".", "#", ".", "#" },
+    { "#", ".", ".", ".", ".", "#" },
+    { "#", "#", "#", "#", "#", "#" },
+};
+
+List<Tile> visitedTiles = new();
+
+Console.ForegroundColor = ConsoleColor.DarkGray;
+for (int i = 0; i < map.GetLength(0); i++)
+{
+    for (int j = 0; j < map.GetLength(1); j++)
+    {
+        var tile = map[i, j];
+        SetColor(tile);
+        Console.Write($"{tile} ");
+    }
+    Console.Write("\n");
+}
+
+void SetColor(string tile)
+{
+    if (tile == "@")
+        Console.ForegroundColor = ConsoleColor.Red;
+    else
+        Console.ForegroundColor = ConsoleColor.DarkGray;
+}
+
+Console.ReadKey();
---
+++ Shadowcasting/Tile.cs
@@ -0,0 +1,4 @@
+public class Tile{
+    public int x;
+    public int y;
+}
